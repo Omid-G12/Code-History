d5373c95c1d312dbfbb1bc31c84de0031246a914
Omid_G12 <omid.12.ghari@gmail.com>
2023-01-24 18:32:25 -0800

    allQuestions static property added to Questions struct

--- Code History/Models/Questions.swift
+++ Code History/Models/Questions.swift
@@ -11,4 +11,23 @@ struct Question {
     let questionText: String
     let possibleAnswers: [String]
     let correctAnswerIndex: Int
+    
+    static var allQuestions = [
+        Question(questionText: "Who invented the World Wide Web?",
+                possibleAnswers: [
+                "Steve Jobs",
+                "Linus Torvalds",
+                "Bill Gates",
+                "Tim Berners-Lee"
+                ],
+                correctAnswerIndex: 3),
+        Question(questionText: "What was the first object oriented programming language?",
+                 possibleAnswers: [
+                 "Simula",
+                 "Python",
+                 "Swift",
+                 "C"
+                 ],
+                 correctAnswerIndex: 0)
+    ]
 }
