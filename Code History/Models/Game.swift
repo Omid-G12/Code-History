51f424391005f7260eddfdcd077c3d4e91866176
Omid_G12 <omid.12.ghari@gmail.com>
2023-01-25 16:31:35 -0800

    add game status update function

--- Code History/Models/Game.swift
+++ Code History/Models/Game.swift
@@ -8,6 +8,20 @@
 import Foundation
 
 struct Game {
+    private(set) var currentQuestionIndex = 0
     private(set) var guesses = [Question: Int]()
+    private(set) var isOver = false
     private let questions = Question.allQuestions.shuffled()
+    
+    mutating func makeGuessForCurrentQuestion(atIndex index: Int) {
+        guesses[currentQuestion] = index
+    }
+    
+    mutating func updateGameStatus() {
+        if currentQuestionIndex < questions.count - 1 {
+            currentQuestionIndex += 1
+        } else {
+            isOver = true
+        }
+    }
 }
