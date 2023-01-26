f28bb69c3570b49dc6542422fca3a63a628ca30f
Omid_G12 <omid.12.ghari@gmail.com>
2023-01-25 16:41:18 -0800

    guess count and more game logic added

--- Code History/Models/Game.swift
+++ Code History/Models/Game.swift
@@ -13,6 +13,26 @@ struct Game {
     private(set) var isOver = false
     private let questions = Question.allQuestions.shuffled()
     
+    var guessCount: (correct: Int, incorrect: Int) {
+        var count: (correct: Int, incorrect: Int) = (0, 0)
+        for (question, guessedIndex) in guesses {
+            if question.correctAnswerIndex == guessedIndex {
+                count.correct += 1
+            } else {
+                count.incorrect += 1
+            }
+        }
+        return count
+    }
+    
+    var numberOfQuestions: Int {
+        questions.count
+    }
+    
+    var currentQuestion: Question {
+        questions[currentQuestionIndex]
+    }
+    
     mutating func makeGuessForCurrentQuestion(atIndex index: Int) {
         guesses[currentQuestion] = index
     }
@@ -24,4 +44,6 @@ struct Game {
             isOver = true
         }
     }
+    
+    
 }
